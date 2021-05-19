6b38af2d2cf565f218fe958a43edb0bd04f5491a
Neelisha Shrestha <80264725+thenelysa@users.noreply.github.com>
2021-05-19 20:05:39 +0545

    3rd-loop
    Multiplication of 2 using for loop.

---
+++ multiplication-of-2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	
+	int n=2, i=1;
+	printf("Multiplication table of 2 \n");
+	for (i=1; i<=10; ++i){
+		
+		printf("%d * %d = %d \n" , n,i,n*i);
+	
+	}
+	
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
