6509eff7540e699d0abfb0d325ffebd815856f79
Neelisha Shrestha <80264725+thenelysa@users.noreply.github.com>
2021-05-19 19:53:52 +0545

    1st loop

---
+++ whileloop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	
+	int i=5; //starting point
+	while(i>=1) //ending point
+	{
+		printf("\nNeelisha Shrestha");
+		i=i-1; // decrement
+	}
+	
+	return 0;
+	
+	
+	
+}
