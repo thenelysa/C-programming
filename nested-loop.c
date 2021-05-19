3dc1d802a8c790e6db7d62bf5ab13ce9e76a59c1
Neelisha Shrestha <80264725+thenelysa@users.noreply.github.com>
2021-05-19 20:12:40 +0545

    Nested-loop

---
+++ nested-loop.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	
+
+	int i=0,j=0;
+	for(i=1;i<=3;i++)
+	{
+		for(j=1;j<=5;j++)
+	{
+	printf("\ta");
+}
+
+	printf("\n");
+	
+}
+return 0;	
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
