68cea0257ea5848c924c19c2a601d5d30d7ea561
Neelisha Shrestha <80264725+thenelysa@users.noreply.github.com>
2021-05-19 20:00:10 +0545

    2nd-loop

---
+++ dowhileloop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i=10;
+	do
+	{
+		printf("\n %d. Neelisha Shrestha",i); //code, statement
+		i++;
+	}while(i<=5); //condition
+	
+	
+return 0;
+	
+	
+}
