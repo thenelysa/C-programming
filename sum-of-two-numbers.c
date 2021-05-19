6d39b137223cd6488f5167238015f1745ef94648
Neelisha Shrestha <80264725+thenelysa@users.noreply.github.com>
2021-05-19 19:54:53 +0545

    Addition.

---
+++ sum-of-two-numbers.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+
+{
+	int num1, num2, sum;
+	printf("Enter first number : ");
+	scanf("%d",&num1);
+		printf("Enter second number : ");
+		scanf("%d",&num2);
+		
+		sum=num1+num2;
+		
+		printf("The sum is %d", sum);
+		
+	return 0;
+		
+		
+		
+
+	
+	
+	
+	
+}
