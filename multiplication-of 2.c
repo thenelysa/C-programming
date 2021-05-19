c3b785900ef825c2dea38eee0db116a6ee77b49f
Neelisha Shrestha <80264725+thenelysa@users.noreply.github.com>
2021-05-19 20:02:40 +0545

    1st-loop
    multiplication of 2 using while loop.

---
+++ multiplication-of 2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+
+{
+	
+	int i=1, product;
+	while (i<=10)
+	{
+		product = 2*i;
+		printf(" 2 * %d = %d\n", i, product);
+		i++;
+	}
+	
+	return 0;
+	
+}
